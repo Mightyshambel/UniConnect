5765fd45aa36be18fc38a63d997984db52642f69
unknown <shambelmighty51@gmail.com>
2024-06-30 07:48:28 +0200

    my first commit

---
+++ src/app/layout/authlayout/authlayout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-authlayout',
+  standalone: true,
+  imports: [],
+  templateUrl: './authlayout.component.html',
+  styleUrl: './authlayout.component.scss'
+})
+export class AuthlayoutComponent {
+
+}
---
+++ src/app/layout/homelayout/homelayout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-homelayout',
+  standalone: true,
+  imports: [],
+  templateUrl: './homelayout.component.html',
+  styleUrl: './homelayout.component.scss'
+})
+export class HomelayoutComponent {
+
+}
