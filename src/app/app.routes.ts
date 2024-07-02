facc8fdae5bbb7e90c22dca505a4232dcf7604b0
unknown <shambelmighty51@gmail.com>
2024-07-02 22:23:21 +0200

    my home layout sections

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,33 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { HomelayoutComponent } from './layout/homelayout/homelayout.component';
 import { AuthlayoutComponent } from './layout/authlayout/authlayout.component';
+import { HomeComponent } from './modules/pages/home/home.component';
+import { AboutComponent } from './modules/pages/about/about.component';
+import { ServicesComponent } from './modules/pages/services/services.component';
+import { ContactusComponent } from './modules/pages/contactus/contactus.component';
+import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
 
-    { path: '', component: HomelayoutComponent },
-    { path: 'auth', component:AuthlayoutComponent},
+   
+    { path: 'auth', component: AuthlayoutComponent },
+    {
+        path: '',
+        component: HomelayoutComponent,
+        children: [
+          { path: '', pathMatch: 'full', redirectTo: 'home' },
+          { path: 'home', component: HomeComponent },
+          { path: 'about', component: AboutComponent },
+          { path: 'services', component: ServicesComponent },
+          { path: 'blog', component: AboutComponent },
+          { path: 'contact', component: ContactusComponent }
+        ]
+      }
     
 ];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/layout/homelayout/homelayout.component.ts
+++ src/app/layout/homelayout/homelayout.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../../modules/pages/header/header.component";
+import { FooterComponent } from "../../modules/pages/footer/footer.component";
 
 @Component({
     selector: 'app-homelayout',
     standalone: true,
-    imports:[],
     templateUrl: './homelayout.component.html',
     styleUrl: './homelayout.component.scss',
+    imports: [HeaderComponent, FooterComponent]
 })
 export class HomelayoutComponent
 {
