b66b0e6fe71cf6315d46f4d1d8cdcdf59f9c3727
unknown <shambelmighty51@gmail.com>
2024-07-02 09:26:06 +0200

    home layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomelayoutComponent } from './layout/homelayout/homelayout.component';
+import { AuthlayoutComponent } from './layout/authlayout/authlayout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { path: '', component: HomelayoutComponent },
+    { path: 'auth', component:AuthlayoutComponent},
+    
+];
--- src/app/layout/homelayout/homelayout.component.ts
+++ src/app/layout/homelayout/homelayout.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-homelayout',
-  standalone: true,
-  imports: [],
-  templateUrl: './homelayout.component.html',
-  styleUrl: './homelayout.component.scss'
+    selector: 'app-homelayout',
+    standalone: true,
+    imports:[],
+    templateUrl: './homelayout.component.html',
+    styleUrl: './homelayout.component.scss',
 })
-export class HomelayoutComponent {
-
+export class HomelayoutComponent
+{
+   
 }
