0d090fe6308b98855aa27a240bc9183ed78e238c
unknown <shambelmighty51@gmail.com>
2024-07-03 09:26:08 +0200

    landing page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+
 import { HomelayoutComponent } from './layout/homelayout/homelayout.component';
 import { AuthlayoutComponent } from './layout/authlayout/authlayout.component';
 import { HomeComponent } from './modules/pages/home/home.component';
 import { AboutComponent } from './modules/pages/about/about.component';
 import { ServicesComponent } from './modules/pages/services/services.component';
 import { ContactusComponent } from './modules/pages/contactus/contactus.component';
-import { NgModule } from '@angular/core';
+
 
 export const routes: Routes = [
 
--- src/app/layout/homelayout/homelayout.component.ts
+++ src/app/layout/homelayout/homelayout.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../modules/pages/header/header.component";
 import { FooterComponent } from "../../modules/pages/footer/footer.component";
+import { AboutComponent } from "../../modules/pages/about/about.component";
+import { ServicesComponent } from "../../modules/pages/services/services.component";
+import { BlogsComponent } from "../../modules/pages/blogs/blogs.component";
+import { ContactusComponent } from "../../modules/pages/contactus/contactus.component";
+import { LandingComponent } from "../../modules/pages/landing/landing.component";
 
 @Component({
     selector: 'app-homelayout',
     standalone: true,
     templateUrl: './homelayout.component.html',
     styleUrl: './homelayout.component.scss',
-    imports: [HeaderComponent, FooterComponent]
+    imports: [HeaderComponent, FooterComponent, AboutComponent, ServicesComponent, BlogsComponent, ContactusComponent, LandingComponent]
 })
 export class HomelayoutComponent
 {
