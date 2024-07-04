6b8d91916511ce40403435d301b9c1b3be6f199f
unknown <shambelmighty51@gmail.com>
2024-07-04 23:02:10 +0200

    about page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { HomelayoutComponent } from './layout/homelayout/homelayout.component';
 import { AuthlayoutComponent } from './layout/authlayout/authlayout.component';
-import { HomeComponent } from './modules/pages/home/home.component';
+
 import { AboutComponent } from './modules/pages/about/about.component';
 import { ServicesComponent } from './modules/pages/services/services.component';
 import { ContactusComponent } from './modules/pages/contactus/contactus.component';
@@ -18,7 +18,6 @@ export const routes: Routes = [
         component: HomelayoutComponent,
         children: [
           { path: '', pathMatch: 'full', redirectTo: 'home' },
-          { path: 'home', component: HomeComponent },
           { path: 'about', component: AboutComponent },
           { path: 'services', component: ServicesComponent },
           { path: 'blog', component: AboutComponent },
