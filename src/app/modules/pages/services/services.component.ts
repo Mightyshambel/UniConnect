dfec271c2536cfd83de430effb668c0aafc63acc
unknown <shambelmighty51@gmail.com>
2024-08-01 09:34:09 +0200

    header and about us

--- src/app/modules/pages/services/services.component.ts
+++ src/app/modules/pages/services/services.component.ts
@@ -8,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrl: './services.component.scss'
 })
 export class ServicesComponent {
-
+  sections = [
+    {
+      icon: 'path/to/icon1.svg',
+      title: 'WHY COLLEGE MATTERS',
+      description: 'College is worth it. Your future is worth it. You are worth it.',
+      link: '#',
+      linkText: 'Learn why college matters >'
+    },
+    {
+      icon: 'path/to/icon2.svg',
+      title: 'PAYING FOR COLLEGE',
+      description: 'There are lots of options available to you. We can help you find them.',
+      link: '#',
+      linkText: 'Learn how to pay for college >'
+    },
+    {
+      icon: 'path/to/icon3.svg',
+      title: 'YOUR COLLEGE ROADMAP',
+      description: 'The path may seem unclear. We can help you find your way.',
+      link: '#',
+      linkText: 'Learn how to get to college >'
+    }
+  ];
 }
